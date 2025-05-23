31bc3fa688d2209e2e3e72538d4748b9d37cfba8
Baidyanath Kundu <kundubaidya99@gmail.com>
2025-05-24 00:17:03 +0200

    Add validation mode for FAv3 deterministic backward pass
    Also added logging of environment variables

--- training/attn_impl/flash_attn.py
+++ training/attn_impl/flash_attn.py
@@ -1,5 +1,12 @@
 from flash_attn_interface import flash_attn_func
+import os
+from training.utils import logger
+
+if os.environ.get("VALIDATION_MODE") == "1":
+    det_flash_attn = True
+else:
+    det_flash_attn = False
 
 def attention(q, k, v, is_causal):
-    output, _ = flash_attn_func(q, k, v, causal=is_causal)
+    output, _ = flash_attn_func(q, k, v, causal=is_causal, deterministic=det_flash_attn)
     return output
